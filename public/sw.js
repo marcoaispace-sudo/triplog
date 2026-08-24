e8d15460cd1e130440f8b899e91598dfc36d71a6
marcoaispace-sudo <marcoaispace@gmail.com>
2026-08-24 16:28:19 +0800

    Add trip-specific itinerary and preparation lists

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "triplog-v3";
+const CACHE_NAME = "triplog-v4";
 const APP_SHELL = ["./", "./manifest.webmanifest", "./favicon.svg"];
 
 self.addEventListener("install", (event) => {
