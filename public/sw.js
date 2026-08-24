785d55b448b7b548e8ec2bb9f9c4b3cad2ff5b4a
marcoaispace-sudo <marcoaispace@gmail.com>
2026-08-24 21:08:19 +0800

    Add private trip document uploads
    Reset itinerary to day one and securely manage ticket and hotel files in private Supabase Storage.

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "triplog-v4";
+const CACHE_NAME = "triplog-v5";
 const APP_SHELL = ["./", "./manifest.webmanifest", "./favicon.svg"];
 
 self.addEventListener("install", (event) => {
