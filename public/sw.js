57e220c8260cd8b67574f6a2a52d97221d7c1c0b
marcoaispace-sudo <marcoaispace@gmail.com>
2026-08-25 21:39:32 +0800

    Make backup itinerary shared across all days

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "triplog-v11";
+const CACHE_NAME = "triplog-v12";
 const APP_SHELL = ["./", "./manifest.webmanifest", "./favicon.svg"];
 
 self.addEventListener("install", (event) => {
