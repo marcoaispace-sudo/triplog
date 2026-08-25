37e98e0f16be661d09fd198991470778e3d811db
marcoaispace-sudo <marcoaispace@gmail.com>
2026-08-25 22:59:07 +0800

    Confirm before deleting itinerary items

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "triplog-v12";
+const CACHE_NAME = "triplog-v13";
 const APP_SHELL = ["./", "./manifest.webmanifest", "./favicon.svg"];
 
 self.addEventListener("install", (event) => {
