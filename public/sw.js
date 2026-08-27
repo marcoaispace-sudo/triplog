90b43f48bd7cba4c7af08ce7f83a36ee256803f8
marcoaispace-sudo <marcoaispace@gmail.com>
2026-08-27 20:13:43 +0800

    Fix homepage itinerary display
    TripLog v0.18

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "triplog-v17";
+const CACHE_NAME = "triplog-v18";
 const APP_SHELL = ["./", "./manifest.webmanifest", "./favicon.svg"];
 
 self.addEventListener("install", (event) => {
