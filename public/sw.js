851331ffb2eb243586b058da065b49df5f184d7c
marcoaispace-sudo <marcoaispace@gmail.com>
2026-08-27 20:40:46 +0800

    Update public/sw.js for TripLog v0.20 expenses

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "triplog-v19";
+const CACHE_NAME = "triplog-v20";
 const APP_SHELL = ["./", "./manifest.webmanifest", "./favicon.svg"];
 
 self.addEventListener("install", (event) => {
