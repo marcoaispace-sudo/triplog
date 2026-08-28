52df7bc77a391b07266f43be93020db9a33f2095
marcoaispace-sudo <marcoaispace@gmail.com>
2026-08-28 13:54:00 +0800

    Add Google and Naver map selection
    * Add map provider choice (app/page.tsx)
    
    * Add map provider choice (app/globals.css)
    
    * Add map provider choice (app/google-places.ts)
    
    * Add map provider choice (public/sw.js)

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "triplog-v26";
+const CACHE_NAME = "triplog-v27";
 const APP_SHELL = ["./", "./manifest.webmanifest", "./favicon.svg"];
 
 self.addEventListener("install", (event) => {
