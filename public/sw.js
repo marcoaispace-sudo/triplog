5c3154fc4a41171bc79c25042ad2ff704d6dca5c
marcoaispace-sudo <marcoaispace@gmail.com>
2026-08-28 13:31:19 +0800

    Add itinerary directions (public/sw.js)

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "triplog-v25";
+const CACHE_NAME = "triplog-v26";
 const APP_SHELL = ["./", "./manifest.webmanifest", "./favicon.svg"];
 
 self.addEventListener("install", (event) => {
