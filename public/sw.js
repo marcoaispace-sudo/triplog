2c080d06307fe3132589092bc0e240e4009e0be8
marcoaispace-sudo <marcoaispace@gmail.com>
2026-08-28 12:54:43 +0800

    新增帳目詳情及私人收據原圖
    * 新增帳目詳情及收據原圖
    
    * 加入帳目詳情及收據樣式
    
    * 更新離線快取版本

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "triplog-v23";
+const CACHE_NAME = "triplog-v24";
 const APP_SHELL = ["./", "./manifest.webmanifest", "./favicon.svg"];
 
 self.addEventListener("install", (event) => {
