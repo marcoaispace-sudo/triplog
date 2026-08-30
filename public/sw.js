1f5206fce44e19fafa8ce7a4b1e5557836c9f1e2
marcoaispace-sudo <marcoaispace@gmail.com>
2026-08-30 17:14:15 +0800

    Make companion hotel details read-only

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "triplog-v28";
+const CACHE_NAME = "triplog-v29";
 const APP_SHELL = ["./", "./manifest.webmanifest", "./favicon.svg"];
 
 self.addEventListener("install", (event) => {
