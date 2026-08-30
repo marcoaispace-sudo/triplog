70e4582033191cf01725a9151c700b8fbde08490
marcoaispace-sudo <marcoaispace@gmail.com>
2026-08-30 17:59:01 +0800

    Simplify shared packing checklist

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "triplog-v29";
+const CACHE_NAME = "triplog-v30";
 const APP_SHELL = ["./", "./manifest.webmanifest", "./favicon.svg"];
 
 self.addEventListener("install", (event) => {
