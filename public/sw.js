b2eb60bcc35beb8810a0dc681af2fe8d2f1e468b
marcoaispace-sudo <marcoaispace@gmail.com>
2026-08-31 22:05:25 +0800

    Add delete action to shared packing list

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "triplog-v32";
+const CACHE_NAME = "triplog-v33";
 const APP_SHELL = ["./", "./manifest.webmanifest", "./favicon.svg"];
 
 self.addEventListener("install", (event) => {
