3abac81a66731a003dd1d03df6005cc07f02f3a9
marcoaispace-sudo <marcoaispace@gmail.com>
2026-09-01 17:01:00 +0800

    Add offline travel quick information card

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "triplog-v33";
+const CACHE_NAME = "triplog-v34";
 const APP_SHELL = ["./", "./manifest.webmanifest", "./favicon.svg"];
 
 self.addEventListener("install", (event) => {
