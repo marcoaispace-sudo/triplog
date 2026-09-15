1f4f3a1932817452ae411df7f8fa3a00a31030eb
marcoaispace-sudo <marcoaispace@gmail.com>
2026-09-15 22:28:06 +0800

    Clear homepage after completed trips

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "triplog-v37";
+const CACHE_NAME = "triplog-v38";
 const APP_SHELL = ["./", "./manifest.webmanifest", "./favicon.svg"];
 
 self.addEventListener("install", (event) => {
