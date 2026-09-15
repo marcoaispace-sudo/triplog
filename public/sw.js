f323583cac4a68d5a6c802d04897c286cc02d9e3
marcoaispace-sudo <marcoaispace@gmail.com>
2026-09-15 22:02:56 +0800

    Add post-trip journey review

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "triplog-v36";
+const CACHE_NAME = "triplog-v37";
 const APP_SHELL = ["./", "./manifest.webmanifest", "./favicon.svg"];
 
 self.addEventListener("install", (event) => {
